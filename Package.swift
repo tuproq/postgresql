86c71fd0539b0356d8a5e40ac259259c4133f98b
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2021-11-21 12:39:05 +0500

    Bump swift-tools-version to 5.5

--- Package.swift
+++ Package.swift
@@ -1,19 +1,35 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.5
 
 import PackageDescription
 
 let package = Package(
     name: "tuproq-postgresql",
+    platforms: [
+        .iOS(.v15),
+        .macOS(.v12),
+        .tvOS(.v15),
+        .watchOS(.v8)
+    ],
     products: [
         .library(name: "PostgreSQL", targets: ["PostgreSQL"])
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-log.git", from: "1.2.0"),
-        .package(url: "https://github.com/apple/swift-nio.git", from: "2.17.0"),
-        .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "2.7.2")
+        .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
+        .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "2.0.0")
     ],
     targets: [
-        .target(name: "PostgreSQL", dependencies: ["Logging", "NIO", "NIOSSL"]),
-        .testTarget(name: "PostgreSQLTests", dependencies: ["PostgreSQL"])
-    ]
+        .target(name: "PostgreSQL", dependencies: [
+            .product(name: "Logging", package: "swift-log"),
+            .product(name: "NIO", package: "swift-nio"),
+            .product(name: "NIOSSL", package: "swift-nio-ssl")
+        ]),
+        .executableTarget(name: "PostgreSQLExample", dependencies: [
+            .target(name: "PostgreSQL")
+        ]),
+        .testTarget(name: "PostgreSQLTests", dependencies: [
+            .target(name: "PostgreSQL")
+        ])
+    ],
+    swiftLanguageVersions: [.v5]
 )
