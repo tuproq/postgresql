c189d9e346faa61788a5d2a07f72ddbefea4a235
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2021-12-26 11:57:20 +0100

    Create a common MessageType protocol for all PostgreSQL messages

---
+++ Sources/PostgreSQL/Request/Message/MesageType.swift
@@ -0,0 +1,11 @@
+import NIOCore
+
+protocol MessageType {
+    var identifier: Message.Identifier { get }
+
+    func write(into buffer: inout ByteBuffer)
+}
+
+extension MessageType {
+    func write(into buffer: inout ByteBuffer) {}
+}
