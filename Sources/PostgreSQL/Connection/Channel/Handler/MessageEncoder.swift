cd2be3baabbccba8cdf6484ccca0036c5be2b56d
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2021-12-27 22:16:16 +0100

    Create MessageEncoder

---
+++ Sources/PostgreSQL/Connection/Channel/Handler/MessageEncoder.swift
@@ -0,0 +1,22 @@
+import NIOCore
+
+final class MessageEncoder: MessageToByteEncoder {
+    typealias OutboundIn = Message
+
+    func encode(data: Message, out: inout ByteBuffer) throws {
+        var message = data
+
+        switch message.identifier {
+        case .none: break
+        default:
+            if let value = message.identifier.value {
+                out.writeInteger(value)
+            }
+        }
+
+        let writerIndex = out.writerIndex
+        out.moveWriterIndex(forwardBy: 4)
+        out.writeBuffer(&message.buffer)
+        out.setInteger(Int32(out.writerIndex - writerIndex), at: writerIndex)
+    }
+}
