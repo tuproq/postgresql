d657f3bf3320f76753cf7d7181751d01aa6bfa1f
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2022-01-02 12:15:10 +0100

    Create Password message type

---
+++ Sources/PostgreSQL/Request/Message/Type/Message+Password.swift
@@ -0,0 +1,16 @@
+import NIOCore
+
+extension Message {
+    struct Password: MessageType {
+        let identifier: Identifier = .password
+        var value: String
+
+        init(_ value: String) {
+            self.value = value
+        }
+
+        func write(into buffer: inout ByteBuffer) {
+            buffer.writeString(value + "\0")
+        }
+    }
+}
