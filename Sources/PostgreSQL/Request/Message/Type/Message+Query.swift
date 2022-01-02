5c9514ca25ae42632067f29e96be13babf5fcb7a
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2022-01-02 12:15:44 +0100

    Create Query message type

---
+++ Sources/PostgreSQL/Request/Message/Type/Message+Query.swift
@@ -0,0 +1,16 @@
+import NIOCore
+
+extension Message {
+    struct Query: MessageType {
+        let identifier: Identifier = .query
+        var string: String
+
+        init(_ string: String) {
+            self.string = string
+        }
+
+        func write(into buffer: inout ByteBuffer) {
+            buffer.writeString(string + "\0")
+        }
+    }
+}
