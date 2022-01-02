3ec069b9fbb3787ee2e9a8e9265d830e365ca464
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2022-01-02 12:21:22 +0100

    Refactor

--- Sources/PostgreSQL/Request/Message/Type/Message+RowDescription.swift
+++ Sources/PostgreSQL/Request/Message/Type/Message+RowDescription.swift
@@ -7,7 +7,7 @@ extension Message {
 
         init(buffer: inout ByteBuffer) throws {
             guard let columns = try buffer.readArray(as: Column.self, { buffer in
-                return try Column(buffer: &buffer)
+                return try .init(buffer: &buffer)
             }) else {
                 throw MessageError("Can't parse row description columns.")
             }
