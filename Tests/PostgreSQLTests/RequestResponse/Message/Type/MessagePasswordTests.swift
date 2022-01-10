708619f5dda67d5d16dee9941f106b11018f5a49
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2022-01-10 19:43:55 +0100

    Test Message.Password write into ByteBuffer

--- Tests/PostgreSQLTests/RequestResponse/Message/Type/MessagePasswordTests.swift
+++ Tests/PostgreSQLTests/RequestResponse/Message/Type/MessagePasswordTests.swift
@@ -13,4 +13,16 @@ final class MessagePasswordTests: XCTestCase {
         // Assert
         XCTAssertEqual(messageType.value, value)
     }
+
+    func testWrite() {
+        // Arrange
+        let messageType = Message.Password("password")
+        var buffer = ByteBufferAllocator().buffer(capacity: 0)
+
+        // Act
+        messageType.write(into: &buffer)
+
+        // Assert
+        XCTAssertEqual(buffer.getString(at: 0, length: buffer.readableBytes), "\(messageType.value)\0")
+    }
 }
