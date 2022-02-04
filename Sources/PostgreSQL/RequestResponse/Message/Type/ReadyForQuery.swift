519b7bc5916708cf38770d0b230e2422eaa6704a
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2022-02-04 18:25:12 +0500

    Create Message.ReadyForQuery

---
+++ Sources/PostgreSQL/RequestResponse/Message/Type/ReadyForQuery.swift
@@ -0,0 +1,23 @@
+import NIOCore
+
+extension Message {
+    struct ReadyForQuery: MessageType {
+        let identifier: Identifier = .readyForQuery
+        let status: Status
+
+        init(buffer: inout ByteBuffer) throws {
+            guard let value = buffer.readInteger(as: UInt8.self), let status = Status(rawValue: value) else {
+                throw MessageError("Can't parse transaction status.")
+            }
+            self.status = status
+        }
+    }
+}
+
+extension Message.ReadyForQuery {
+    enum Status: UInt8 {
+        case idle = 0x49 // 'I'
+        case transaction = 0x54 // 'T'
+        case transactionFailed = 0x45 // 'E'
+    }
+}
---
+++ Tests/PostgreSQLTests/RequestResponse/Message/Type/MessageReadyForQueryTests.swift
@@ -0,0 +1,35 @@
+import NIOCore
+@testable import PostgreSQL
+import XCTest
+
+final class MessageReadyForQueryTests: XCTestCase {
+    func testInit() {
+        // Arrange
+        let bufferAllocator = ByteBufferAllocator()
+        var buffer = bufferAllocator.buffer(capacity: 0)
+
+        // Act/Assert
+        XCTAssertThrowsError(try Message.ReadyForQuery(buffer: &buffer)) { error in
+            XCTAssertEqual(error.localizedDescription, "Can't parse transaction status.")
+        }
+
+        // Arrange
+        let status: Message.ReadyForQuery.Status = .idle
+        buffer = bufferAllocator.buffer(capacity: 0)
+        buffer.writeInteger(status.rawValue)
+
+        // Act
+        let messageType = try! Message.ReadyForQuery(buffer: &buffer)
+
+        // Assert
+        XCTAssertEqual(messageType.identifier, .readyForQuery)
+        XCTAssertEqual(messageType.status, status)
+    }
+
+    func testStatuses() {
+        // Assert
+        XCTAssertEqual(Message.ReadyForQuery.Status.idle.rawValue, 0x49)
+        XCTAssertEqual(Message.ReadyForQuery.Status.transaction.rawValue, 0x54)
+        XCTAssertEqual(Message.ReadyForQuery.Status.transactionFailed.rawValue, 0x45)
+    }
+}
