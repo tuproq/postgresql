39e7357fe847cf91109ab446f92e76c8ee168c5c
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2022-02-05 15:10:35 +0500

    Create Message.NotificationResponse

---
+++ Sources/PostgreSQL/RequestResponse/Message/Type/NotificationResponse.swift
@@ -0,0 +1,25 @@
+import NIOCore
+
+extension Message {
+    struct NotificationResponse: MessageType {
+        let identifier: Identifier = .notificationResponse
+        let processID: Int32
+        let channel: String
+        let payload: String
+
+        init(buffer: inout ByteBuffer) throws {
+            guard let processID: Int32 = buffer.readInteger() else {
+                throw MessageError("Can't parse process ID.")
+            }
+            guard let channel = buffer.readNullTerminatedString() else {
+                throw MessageError("Can't parse channel.")
+            }
+            guard let payload = buffer.readNullTerminatedString() else {
+                throw MessageError("Can't parse payload.")
+            }
+            self.processID = processID
+            self.channel = channel
+            self.payload = payload
+        }
+    }
+}
---
+++ Tests/PostgreSQLTests/RequestResponse/Message/Type/NotificationResponseTests.swift
@@ -0,0 +1,53 @@
+import NIOCore
+@testable import PostgreSQL
+import XCTest
+
+final class MessageNotificationResponseTests: XCTestCase {
+    func testInit() {
+        // Arrange
+        let bufferAllocator = ByteBufferAllocator()
+        var buffer = bufferAllocator.buffer(capacity: 0)
+
+        // Act/Assert
+        XCTAssertThrowsError(try Message.NotificationResponse(buffer: &buffer)) { error in
+            XCTAssertEqual(error.localizedDescription, "Can't parse process ID.")
+        }
+
+        // Arrange
+        let processID: Int32 = 1
+        buffer = bufferAllocator.buffer(capacity: 0)
+        buffer.writeInteger(processID)
+
+        // Act/Assert
+        XCTAssertThrowsError(try Message.NotificationResponse(buffer: &buffer)) { error in
+            XCTAssertEqual(error.localizedDescription, "Can't parse channel.")
+        }
+
+        // Arrange
+        let channel = "channel"
+        buffer = bufferAllocator.buffer(capacity: 0)
+        buffer.writeInteger(processID)
+        buffer.writeNullTerminatedString(channel)
+
+        // Act/Assert
+        XCTAssertThrowsError(try Message.NotificationResponse(buffer: &buffer)) { error in
+            XCTAssertEqual(error.localizedDescription, "Can't parse payload.")
+        }
+
+        // Arrange
+        let payload = "payload"
+        buffer = bufferAllocator.buffer(capacity: 0)
+        buffer.writeInteger(processID)
+        buffer.writeNullTerminatedString(channel)
+        buffer.writeNullTerminatedString(payload)
+
+        // Act
+        let messageType = try! Message.NotificationResponse(buffer: &buffer)
+
+        // Assert
+        XCTAssertEqual(messageType.identifier, .notificationResponse)
+        XCTAssertEqual(messageType.processID, processID)
+        XCTAssertEqual(messageType.channel, channel)
+        XCTAssertEqual(messageType.payload, payload)
+    }
+}
