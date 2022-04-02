fe0568059115c8a78c64fff15c9d4f4b21949ed8
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2022-04-02 16:33:57 +0500

    Test Data type

--- Tests/PostgreSQLTests/RequestResponse/Message/Data/DataTests.swift
+++ Tests/PostgreSQLTests/RequestResponse/Message/Data/DataTests.swift
@@ -1,8 +1,24 @@
-//
-//  File.swift
-//  
-//
-//  Created by Sukhrob Khakimov on 02/04/22.
-//
+import NIOCore
+@testable import PostgreSQL
+import XCTest
 
-import Foundation
+final class DataTests: BaseTests {
+    func testDefaultFormatAndType() {
+        // Assert
+        XCTAssertEqual(Data.psqlFormat, .binary)
+        XCTAssertEqual(Data.psqlType, .bytea)
+    }
+
+    func testInit() {
+        // Arrange
+        let format: DataFormat = .binary
+        let value = Data("text".utf8)
+        var expectedValue: Data?
+        var buffer = ByteBuffer()
+        value.encode(into: &buffer, with: format)
+
+        // Act/Assert
+        XCTAssertNoThrow(expectedValue = try Data(buffer: &buffer, format: format))
+        XCTAssertEqual(expectedValue, value)
+    }
+}
