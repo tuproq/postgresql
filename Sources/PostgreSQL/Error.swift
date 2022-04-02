99f2d4ffe1d5f469ae9d562d3bab9612c7ea02bd
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2022-04-02 15:36:29 +0500

    Create ErrorType enum to store all types of errors

--- Sources/PostgreSQL/Error.swift
+++ Sources/PostgreSQL/Error.swift
@@ -8,3 +8,20 @@ struct MessageError: LocalizedError {
         self.message = message
     }
 }
+
+enum ErrorType: CustomStringConvertible {
+    case invalidData(format: DataFormat, type: DataType)
+    case invalidDataType(_ type: DataType)
+    case unknown
+
+    var description: String { message }
+
+    var message: String {
+        switch self {
+        case .invalidData(let format, let type):
+            return "An invalid data for data format `\(format)` and data type `\(type)`."
+        case .invalidDataType(let type): return "An invalid data type `\(type)`."
+        case .unknown: return "An unknown error."
+        }
+    }
+}
