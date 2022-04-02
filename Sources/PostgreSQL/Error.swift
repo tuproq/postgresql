0637c9097a0402f22d589396aa56e579a4692f01
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2022-04-02 15:44:38 +0500

    Create PostgreSQLError

--- Sources/PostgreSQL/Error.swift
+++ Sources/PostgreSQL/Error.swift
@@ -1,5 +1,24 @@
 import Foundation
 
+public struct PostgreSQLError: LocalizedError {
+    let message: String
+    public var errorDescription: String? { message }
+
+    init(_ errorType: ErrorType) {
+        self.init(errorType.message)
+    }
+
+    init(_ message: String? = nil) {
+        let errorType = String(describing: type(of: self))
+
+        if let message = message, !message.isEmpty {
+            self.message = "\(errorType): \(message)"
+        } else {
+            self.message = "\(errorType): \(ErrorType.unknown)"
+        }
+    }
+}
+
 struct MessageError: LocalizedError {
     private let message: String?
     var errorDescription: String? { message }
