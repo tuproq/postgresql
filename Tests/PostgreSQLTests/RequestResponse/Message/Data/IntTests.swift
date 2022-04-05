a0c5b729adbb7e80603504459575fc525b35975a
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2022-04-05 23:33:01 +0500

    Refactor Int tests

--- Tests/PostgreSQLTests/RequestResponse/Message/Data/IntTests.swift
+++ Tests/PostgreSQLTests/RequestResponse/Message/Data/IntTests.swift
@@ -14,6 +14,12 @@ final class IntTests: BaseTests {
         }
 
         // Assert
+        XCTAssertEqual(Int16.psqlFormat, .binary)
+        XCTAssertEqual(Int16.psqlType, .int2)
+        XCTAssertEqual(Int32.psqlFormat, .binary)
+        XCTAssertEqual(Int32.psqlType, .int4)
+        XCTAssertEqual(Int64.psqlFormat, .binary)
+        XCTAssertEqual(Int64.psqlType, .int8)
         XCTAssertEqual(Int.psqlFormat, .binary)
         XCTAssertEqual(Int.psqlType, type)
     }
@@ -21,64 +27,146 @@ final class IntTests: BaseTests {
     func testInitWithInvalidType() {
         // Arrange
         let type: DataType = .uuid
-        let value: Int = 1
-        var buffer = ByteBuffer()
-        value.encode(into: &buffer)
+        let values: [Codable] = [Int16(1), Int32(1), Int64(1), Int(1)]
+        var buffer: ByteBuffer
 
-        // Act/Assert
-        XCTAssertThrowsError(try Int(buffer: &buffer, type: type)) { error in
-            XCTAssertEqual(
-                error.localizedDescription,
-                PostgreSQL.error(.invalidDataType(type)).localizedDescription
-            )
+        for value in values {
+            buffer = ByteBuffer()
+            value.encode(into: &buffer)
+
+            // Act/Assert
+            XCTAssertThrowsError(try Int16(buffer: &buffer, type: type)) { error in
+                XCTAssertEqual(
+                    error.localizedDescription,
+                    PostgreSQL.error(.invalidDataType(type)).localizedDescription
+                )
+            }
+            XCTAssertThrowsError(try Int32(buffer: &buffer, type: type)) { error in
+                XCTAssertEqual(
+                    error.localizedDescription,
+                    PostgreSQL.error(.invalidDataType(type)).localizedDescription
+                )
+            }
+            XCTAssertThrowsError(try Int64(buffer: &buffer, type: type)) { error in
+                XCTAssertEqual(
+                    error.localizedDescription,
+                    PostgreSQL.error(.invalidDataType(type)).localizedDescription
+                )
+            }
+            XCTAssertThrowsError(try Int(buffer: &buffer, type: type)) { error in
+                XCTAssertEqual(
+                    error.localizedDescription,
+                    PostgreSQL.error(.invalidDataType(type)).localizedDescription
+                )
+            }
         }
     }
 
-    func testInitWithValidValues() {
+    func testInitWithValidValue() {
         // Arrange
-        let formats = DataFormat.allCases
+        let value: Int = 1
         var expectedValue: Int?
+        var buffer: ByteBuffer
 
-        for format in formats {
+        for format in DataFormat.allCases {
             // Arrange
-            let value: Int16 = 1
-            let type: DataType = .int2
-            var buffer = ByteBuffer()
-            value.encode(into: &buffer, with: format)
+            buffer = ByteBuffer()
+            Int16(value).encode(into: &buffer, with: format)
 
             // Act/Assert
-            XCTAssertNoThrow(expectedValue = try Int(buffer: &buffer, format: format, type: type))
-            XCTAssertEqual(expectedValue, Int(value))
-        }
+            XCTAssertNoThrow(expectedValue = Int(try Int16(buffer: &buffer, format: format, type: .int2)))
+            XCTAssertEqual(expectedValue, value)
 
-        for format in formats {
             // Arrange
-            let value: Int32 = 1
-            let type: DataType = .int4
-            var buffer = ByteBuffer()
-            value.encode(into: &buffer, with: format)
+            buffer = ByteBuffer()
+            Int16(value).encode(into: &buffer, with: format)
 
             // Act/Assert
-            XCTAssertNoThrow(expectedValue = try Int(buffer: &buffer, format: format, type: type))
-            XCTAssertEqual(expectedValue, Int(value))
-        }
+            XCTAssertNoThrow(expectedValue = Int(try Int16(buffer: &buffer, format: format)))
+            XCTAssertEqual(expectedValue, value)
 
-        for format in formats {
             // Arrange
-            let value: Int64 = 1
-            let type: DataType = .int8
-            var buffer = ByteBuffer()
-            value.encode(into: &buffer, with: format)
+            buffer = ByteBuffer()
+            Int16(value).encode(into: &buffer, with: format)
 
             // Act/Assert
-            XCTAssertNoThrow(expectedValue = try Int(buffer: &buffer, format: format, type: type))
-            XCTAssertEqual(expectedValue, Int(value))
-        }
+            XCTAssertNoThrow(expectedValue = Int(try Int32(buffer: &buffer, format: format, type: .int2)))
+            XCTAssertEqual(expectedValue, value)
 
-        for format in formats {
             // Arrange
-            let value: Int = 1
-            var buffer = ByteBuffer()
+            buffer = ByteBuffer()
+            Int32(value).encode(into: &buffer, with: format)
+
+            // Act/Assert
+            XCTAssertNoThrow(expectedValue = Int(try Int32(buffer: &buffer, format: format, type: .int4)))
+            XCTAssertEqual(expectedValue, value)
+
+            // Arrange
+            buffer = ByteBuffer()
+            Int32(value).encode(into: &buffer, with: format)
+
+            // Act/Assert
+            XCTAssertNoThrow(expectedValue = Int(try Int32(buffer: &buffer, format: format)))
+            XCTAssertEqual(expectedValue, value)
+
+            // Arrange
+            buffer = ByteBuffer()
+            Int16(value).encode(into: &buffer, with: format)
+
+            // Act/Assert
+            XCTAssertNoThrow(expectedValue = Int(try Int64(buffer: &buffer, format: format, type: .int2)))
+            XCTAssertEqual(expectedValue, value)
+
+            // Arrange
+            buffer = ByteBuffer()
+            Int32(value).encode(into: &buffer, with: format)
+
+            // Act/Assert
+            XCTAssertNoThrow(expectedValue = Int(try Int64(buffer: &buffer, format: format, type: .int4)))
+            XCTAssertEqual(expectedValue, value)
+
+            // Arrange
+            buffer = ByteBuffer()
+            Int64(value).encode(into: &buffer, with: format)
+
+            // Act/Assert
+            XCTAssertNoThrow(expectedValue = Int(try Int64(buffer: &buffer, format: format, type: .int8)))
+            XCTAssertEqual(expectedValue, value)
+
+            // Arrange
+            buffer = ByteBuffer()
+            Int64(value).encode(into: &buffer, with: format)
+
+            // Act/Assert
+            XCTAssertNoThrow(expectedValue = Int(try Int64(buffer: &buffer, format: format)))
+            XCTAssertEqual(expectedValue, value)
+
+            // Arrange
+            buffer = ByteBuffer()
+            Int16(value).encode(into: &buffer, with: format)
+
+            // Act/Assert
+            XCTAssertNoThrow(expectedValue = try Int(buffer: &buffer, format: format, type: .int2))
+            XCTAssertEqual(expectedValue, value)
+
+            // Arrange
+            buffer = ByteBuffer()
+            Int32(value).encode(into: &buffer, with: format)
+
+            // Act/Assert
+            XCTAssertNoThrow(expectedValue = try Int(buffer: &buffer, format: format, type: .int4))
+            XCTAssertEqual(expectedValue, value)
+
+            // Arrange
+            buffer = ByteBuffer()
+            Int64(value).encode(into: &buffer, with: format)
+
+            // Act/Assert
+            XCTAssertNoThrow(expectedValue = try Int(buffer: &buffer, format: format, type: .int8))
+            XCTAssertEqual(expectedValue, value)
+
+            // Arrange
+            buffer = ByteBuffer()
             value.encode(into: &buffer, with: format)
 
             // Act/Assert
@@ -87,23 +175,69 @@ final class IntTests: BaseTests {
         }
     }
 
-    func testInitWithInvalidValues() {
+    func testInitWithInvalidValue() {
         // Arrange
         let invalidValue = UUID()
-        let types: [DataType] = [.int2, .int4, .int8]
+        var buffer: ByteBuffer
 
         for format in DataFormat.allCases {
-            for type in types {
-                var buffer = ByteBuffer()
-                invalidValue.encode(into: &buffer, with: format)
+            buffer = ByteBuffer()
+            invalidValue.encode(into: &buffer, with: format)
 
-                // Act/Assert
-                XCTAssertThrowsError(try Int(buffer: &buffer, format: format, type: type)) { error in
-                    XCTAssertEqual(
-                        error.localizedDescription,
-                        PostgreSQL.error(.invalidData(format: format, type: type)).localizedDescription
-                    )
-                }
+            // Act/Assert
+            XCTAssertThrowsError(try Int16(buffer: &buffer, format: format, type: .int2)) { error in
+                XCTAssertEqual(
+                    error.localizedDescription,
+                    PostgreSQL.error(.invalidData(format: format, type: .int2)).localizedDescription
+                )
+            }
+            XCTAssertThrowsError(try Int32(buffer: &buffer, format: format, type: .int2)) { error in
+                XCTAssertEqual(
+                    error.localizedDescription,
+                    PostgreSQL.error(.invalidData(format: format, type: .int2)).localizedDescription
+                )
+            }
+            XCTAssertThrowsError(try Int32(buffer: &buffer, format: format, type: .int4)) { error in
+                XCTAssertEqual(
+                    error.localizedDescription,
+                    PostgreSQL.error(.invalidData(format: format, type: .int4)).localizedDescription
+                )
+            }
+            XCTAssertThrowsError(try Int64(buffer: &buffer, format: format, type: .int2)) { error in
+                XCTAssertEqual(
+                    error.localizedDescription,
+                    PostgreSQL.error(.invalidData(format: format, type: .int2)).localizedDescription
+                )
+            }
+            XCTAssertThrowsError(try Int64(buffer: &buffer, format: format, type: .int4)) { error in
+                XCTAssertEqual(
+                    error.localizedDescription,
+                    PostgreSQL.error(.invalidData(format: format, type: .int4)).localizedDescription
+                )
+            }
+            XCTAssertThrowsError(try Int64(buffer: &buffer, format: format, type: .int8)) { error in
+                XCTAssertEqual(
+                    error.localizedDescription,
+                    PostgreSQL.error(.invalidData(format: format, type: .int8)).localizedDescription
+                )
+            }
+            XCTAssertThrowsError(try Int(buffer: &buffer, format: format, type: .int2)) { error in
+                XCTAssertEqual(
+                    error.localizedDescription,
+                    PostgreSQL.error(.invalidData(format: format, type: .int2)).localizedDescription
+                )
+            }
+            XCTAssertThrowsError(try Int(buffer: &buffer, format: format, type: .int4)) { error in
+                XCTAssertEqual(
+                    error.localizedDescription,
+                    PostgreSQL.error(.invalidData(format: format, type: .int4)).localizedDescription
+                )
+            }
+            XCTAssertThrowsError(try Int(buffer: &buffer, format: format, type: .int8)) { error in
+                XCTAssertEqual(
+                    error.localizedDescription,
+                    PostgreSQL.error(.invalidData(format: format, type: .int8)).localizedDescription
+                )
             }
         }
     }
