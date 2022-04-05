59f2aeeea6034e7df4c7a49a0194574c89ef81b3
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2022-04-05 21:49:58 +0500

    Conform Int32 to Codable protocol

--- Sources/PostgreSQL/RequestResponse/Message/Data/Int.swift
+++ Sources/PostgreSQL/RequestResponse/Message/Data/Int.swift
@@ -30,3 +30,39 @@ extension Int16: Codable {
         }
     }
 }
+
+extension Int32: Codable {
+    public static var psqlType: DataType { .int4 }
+
+    public init(buffer: inout ByteBuffer, format: DataFormat, type: DataType) throws {
+        switch (format, type) {
+        case (.binary, .int2):
+            guard buffer.readableBytes == 2, let value = buffer.readInteger(as: Int16.self) else {
+                throw error(.invalidData(format: format, type: type))
+            }
+            self = Int32(value)
+        case (.binary, .int4):
+            guard buffer.readableBytes == 4, let value = buffer.readInteger(as: Int32.self) else {
+                throw error(.invalidData(format: format, type: type))
+            }
+            self = Int32(value)
+        case (.text, .int2), (.text, .int4):
+            guard let string = buffer.readString(), let value = Int32(string) else {
+                throw error(.invalidData(format: format, type: type))
+            }
+            self = value
+        default: throw error(.invalidDataType(type))
+        }
+    }
+
+    public init(buffer: inout ByteBuffer, format: DataFormat = Self.psqlFormat) throws {
+        try self.init(buffer: &buffer, format: format, type: Self.psqlType)
+    }
+
+    public func encode(into buffer: inout ByteBuffer, with format: DataFormat) {
+        switch format {
+        case .binary: buffer.writeInteger(self, as: Int32.self)
+        case .text: buffer.writeString(String(self))
+        }
+    }
+}
