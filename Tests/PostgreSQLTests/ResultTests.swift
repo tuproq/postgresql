c1a11feafbdbb0123adaa23aa564c23dd15d502e
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2022-05-14 19:25:34 +0500

    Add test to Result

---
+++ Tests/PostgreSQLTests/ResultTests.swift
@@ -0,0 +1,27 @@
+@testable import PostgreSQL
+import XCTest
+
+final class ResultTests: BaseTests {
+    func testInit() {
+        // Arrange
+        var buffer = ByteBuffer()
+        buffer.writeNullTerminatedString("id")
+        buffer.writeInteger(Int32(2))
+        buffer.writeInteger(Int16(1))
+        buffer.writeInteger(DataType.uuid.rawValue)
+        buffer.writeInteger(Int16(16))
+        buffer.writeInteger(Int32(-1))
+        buffer.writeInteger(DataFormat.binary.rawValue)
+
+        let columns = [try! Column(buffer: &buffer)]
+        let data = [["id": UUID()]]
+
+        // Act
+        let result = Result(columns: columns)
+        result.data = data
+
+        // Assert
+        XCTAssertEqual(result.columns, columns)
+        XCTAssertEqual(result.data as? [[String: UUID]], data)
+    }
+}
