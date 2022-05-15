6b125370e2757844ad4f7980ef6c794f761e37f1
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2022-05-15 17:00:57 +0500

    Add test to ErrorType

--- Tests/PostgreSQLTests/ErrorTests.swift
+++ Tests/PostgreSQLTests/ErrorTests.swift
@@ -13,3 +13,30 @@ final class MessageErrorTests: BaseTests {
         XCTAssertEqual(error.errorDescription, message)
     }
 }
+
+final class ErrorTypeTests: BaseTests {
+    func testCases() {
+        // Arrange
+        let dataFormat: DataFormat = .binary
+        let dataTypeID: DataType = .bool
+
+        // Assert
+        XCTAssertEqual(
+            ErrorType.invalidData(format: dataFormat, type: dataTypeID).message,
+            "An invalid data for data format `\(dataFormat)` and data type `\(dataTypeID)`."
+        )
+        XCTAssertEqual(
+            ErrorType.invalidData(format: dataFormat, type: dataTypeID).message,
+            ErrorType.invalidData(format: dataFormat, type: dataTypeID).description
+        )
+
+        XCTAssertEqual(ErrorType.invalidDataType(dataTypeID).message, "An invalid data type `\(dataTypeID)`.")
+        XCTAssertEqual(
+            ErrorType.invalidDataType(dataTypeID).message,
+            ErrorType.invalidDataType(dataTypeID).description
+        )
+
+        XCTAssertEqual(ErrorType.unknown.message, "An unknown error.")
+        XCTAssertEqual(ErrorType.unknown.message, ErrorType.unknown.description)
+    }
+}
