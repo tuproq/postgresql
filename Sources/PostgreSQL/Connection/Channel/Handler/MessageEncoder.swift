50ecbbde78021805f457e9a51d85800f66dce4f7
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2023-05-23 09:59:54 +0500

    Fix formatting

--- Sources/PostgreSQL/Connection/Channel/Handler/MessageEncoder.swift
+++ Sources/PostgreSQL/Connection/Channel/Handler/MessageEncoder.swift
@@ -5,7 +5,11 @@ final class MessageEncoder: MessageToByteEncoder {
 
     func encode(data: Message, out: inout ByteBuffer) throws {
         var message = data
-        if message.identifier != .none { out.writeInteger(message.identifier.value) }
+
+        if message.identifier != .none {
+            out.writeInteger(message.identifier.value)
+        }
+
         let writerIndex = out.writerIndex
         out.moveWriterIndex(forwardBy: 4)
         out.writeBuffer(&message.buffer)
