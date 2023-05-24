1fbe189ed165411f1696b3997d836a4e980666bf
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2023-05-24 10:32:50 +0500

    Override MessageType description to include TransactionStatus for ReadyForQuery message type

--- Sources/PostgreSQL/Message/Type/ReadyForQuery.swift
+++ Sources/PostgreSQL/Message/Type/ReadyForQuery.swift
@@ -2,6 +2,7 @@ extension Message {
     struct ReadyForQuery: MessageType {
         let identifier: Identifier = .readyForQuery
         let status: Status
+        var description: String { "\(identifier) \(status)" }
 
         init(buffer: inout ByteBuffer) throws {
             guard let value = buffer.readInteger(as: UInt8.self), let status = Status(rawValue: value) else {
@@ -13,9 +14,21 @@ extension Message {
 }
 
 extension Message.ReadyForQuery {
-    enum Status: UInt8 {
+    enum Status: UInt8, CustomStringConvertible {
         case idle = 0x49 // 'I'
         case transaction = 0x54 // 'T'
         case transactionFailed = 0x45 // 'E'
+
+        var description: String {
+            let name: String
+
+            switch self {
+            case .idle: name = "Idle"
+            case .transaction: name = "Transaction"
+            case .transactionFailed: name = "TransactionFailed"
+            }
+
+            return "\(name) (\(String(Character(Unicode.Scalar(rawValue)))))"
+        }
     }
 }
