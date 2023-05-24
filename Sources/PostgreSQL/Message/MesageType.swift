afb778294c3c8179eb341a898e6d718a5d9ad17b
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2023-05-24 10:25:40 +0500

    Conform MessageType to CustomStringConvertible for ease of debugging

--- Sources/PostgreSQL/Message/MesageType.swift
+++ Sources/PostgreSQL/Message/MesageType.swift
@@ -1,9 +1,11 @@
-protocol MessageType {
+protocol MessageType: CustomStringConvertible {
     var identifier: Message.Identifier { get }
 
     func encode(into buffer: inout ByteBuffer)
 }
 
 extension MessageType {
+    var description: String { "\(identifier)" }
+
     func encode(into buffer: inout ByteBuffer) {}
 }
