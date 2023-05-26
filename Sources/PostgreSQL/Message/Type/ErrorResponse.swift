866e13b43c42f68b9afc72037c6cb2b78bd54a8b
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2023-05-26 16:20:09 +0500

    Create ErrorResponse message

---
+++ Sources/PostgreSQL/Message/Type/ErrorResponse.swift
@@ -0,0 +1,18 @@
+extension Message {
+    struct ErrorResponse: MessageType {
+        let identifier: Identifier = .errorResponse
+        var fields = [Field: String]()
+
+        init(buffer: inout ByteBuffer) throws {
+            while let value = buffer.readInteger(as: UInt8.self) {
+                if value == 0 {
+                    break
+                }
+                guard let field = Field(rawValue: value), let string = buffer.readNullTerminatedString() else {
+                    throw clientError(.cantParseErrorResponseFields)
+                }
+                fields[field] = string
+            }
+        }
+    }
+}
