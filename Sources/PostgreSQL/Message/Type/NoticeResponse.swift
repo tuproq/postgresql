df9de2b0899096ab63dfdc1653f035d20ee2e651
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2023-05-26 16:21:01 +0500

    Create NoticeResponse message

---
+++ Sources/PostgreSQL/Message/Type/NoticeResponse.swift
@@ -0,0 +1,18 @@
+extension Message {
+    struct NoticeResponse: MessageType {
+        let identifier: Identifier = .noticeResponse
+        var fields = [Field: String]()
+
+        init(buffer: inout ByteBuffer) throws {
+            while let value = buffer.readInteger(as: UInt8.self) {
+                if value == 0 {
+                    break
+                }
+                guard let field = Field(rawValue: value), let string = buffer.readNullTerminatedString() else {
+                    throw clientError(.cantParseNoticeResponseFields)
+                }
+                fields[field] = string
+            }
+        }
+    }
+}
